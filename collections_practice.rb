903fc04855989c17c9dc44e11bca92d5a0e62c54
Nancy Jo <enciie@gmail.com>
2018-05-01 23:54:48 +0000

    automatically backed up by learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -36,7 +36,9 @@ def kesha_maker(array)
 end
 
 def find_a(array)
-  array.select {|word| word.start_with?("a")}
+  array.select do |word| 
+    word.start_with?("a")
+  end
 end
 
 # def sum_array(array)
